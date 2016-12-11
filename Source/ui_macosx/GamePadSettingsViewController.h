59edbf22fc7200c8cde0f9e4434f3c9746a15985
C.W. Betts <computers57@hotmail.com>
2016-12-11 01:38:15 -0700

    Initial controller Preferences.

---
+++ Source/ui_macosx/GamePadSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  GamePadViewController.h
+//  Play
+//
+//  Created by C.W. Betts on 12/11/16.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GamePadSettingsViewController : NSViewController
+
+@end
---
+++ Source/ui_macosx/GamePadSettingsViewController.mm
@@ -0,0 +1,22 @@
+//
+//  GamePadViewController.m
+//  Play
+//
+//  Created by C.W. Betts on 12/11/16.
+//
+//
+
+#import "GamePadSettingsViewController.h"
+
+@interface GamePadSettingsViewController ()
+
+@end
+
+@implementation GamePadSettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
